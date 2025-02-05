93e9e262eea0cd787763f3ead08617c1c64ec3c9
nikhitha270804 <nikhithapanasa8@gmail.com>
2025-02-05 12:11:38 +0530

    Add files via upload

--- State.jsx
+++ State.jsx
@@ -1,20 +1,92 @@
-import React from 'react'
-import { useState } from 'react'
+// // // import React from 'react';
+// // // import { useState } from 'react'
 
+// // // const State = () => {
+// // //     let [name , setName] = useState("10")
+// // //     console.log(name);
+// // //     console.log(setName);
+// // //     let demo = () => {
+// // //         setName("20")
+// // //     }
+
+// // //   return (
+// // //     <div>
+// // //         <h1>{name}</h1>
+// // //         <button onClick={demo}>click</button>
+// // //     </div>
+    
+// // //   )
+// // // }
+
+// // // export default State
+// // import React, { useState } from 'react'
+
+// // const State = () => {
+// //   let [state ,setState] = useState(0)
+
+// //   let incr = () => {
+// //     setState(state + 1)
+// //   }
+// //   let reset = () => {
+// //     setState(0)
+// //   }
+// //   let decr = () => {
+// //     setState(state - 1)
+// //   }
+// //   return (
+// //     <>
+// //     <h1>{state}</h1>
+// //     <button onClick={incr}>increment</button>
+// //     <button onClick={reset}>reset</button>
+// //     <button onClick={decr}>decrement</button>
+// //     </>
+// //   )
+// // }
+
+// // export default State
+// import React, { useState } from 'react'
+
+// const State = () => {
+//   let [name ,setName] =useState('')
+//   let [password ,setPassword] = useState('')
+//   let demo =(e) => {
+//     e.preventDefault()
+//     console.log({name , password});
+//     name === "admin" && password=== '12345' ?
+//     window.open("http://www.pixabay.com") : console.log("error")
+//   }
+  
+//   return (
+//   <>
+//   <form action="">
+//   <label htmlFor="name">Name:</label>
+//     <input type="text" name="name" id="name" value={name} 
+//     onChange={(e) => {setName(e.target.value)}}/>
+//     <br/>
+//     <label htmlFor="password">Password</label>
+//     <input type="password" name="password" id="password" value={password}
+//     onChange={(e) => {setPassword(e.target.value)}}/>
+//     <br />
+//     <button onClick={demo}>submit</button>
+//   </form>
+//   </>
+//   )
+// }
+
+// export default State
+import React, { useState } from 'react'
+import video from "/1.mp4"
 const State = () => {
-    let [name , setName] = useState("10")
-    console.log(name);
-    console.log(setName);
-    let demo = () => {
-        setName("20")
-    }
-
+  let [state , setState] = useState(true)
+  let demo = () =>{
+    setState(!state)
+    state === true ? ref1.current.play() : ref1.current.pause()
+  }
   return (
     <div>
-        <h1>{name}</h1>
-        <button onClick={demo}>click</button>
-    </div>
-    
+      <video src={video} height="300px" ref={ref1}></video>
+      <button onClick={demo}> click</button>
+      </div>
   )
 }
 
